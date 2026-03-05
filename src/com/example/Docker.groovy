c0bd8d76da02371da8376b5ad3a5623c1e49c13a
Luis Yanez <luisyanez2779@gmail.com>
2026-03-05 14:02:05 -0600

    passing image name

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -38,7 +38,7 @@ class Docker implements Serializable {
     }
 
     def deployToEC2(String imageName) {
-        script.echo "deploying docker image to EC2"
+        script.echo "deploying docker image to EC2 for image ${imageName}"
         def dockerCmd = "docker run -d -p 8080:8080 ${imageName}"
         script.sshagent(['ec2-server-key']) {
             script.sh "ssh -o StrictHostKeyChecking=no ec2-user@18.217.58.32 ${dockerCmd}"
